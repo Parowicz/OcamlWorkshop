fb9f98f0040ae84b6c312d998f11caa7ae10d516
Artur Parowicz <arturparowicz@gmail,com>
2018-08-12 19:58:13 +0200

    Binary Tree formatting.

--- source/binaryTree.ml
+++ source/binaryTree.ml
@@ -47,9 +47,10 @@ module Make (Ord: OrderedType) = struct
   let any = function Empty -> false | _ -> true
   let is_empty = function Empty -> true | _ -> false
 
-  (** @param ele element to append.
-      @return new tree with element appended. 
-                If tree already contains given element, then same tree will be returned.
+  (** 
+    @param ele element to append.
+    @return new tree with element appended. 
+            If tree already contains given element, then same tree will be returned.
   *)
   let rec append ele = function 
     |Empty -> Node(ele, Empty, Empty)
@@ -57,8 +58,9 @@ module Make (Ord: OrderedType) = struct
       if Ord.compare ele value <= 0 then Node(value,  append ele l, r)
       else Node(value, l, append ele r)  
   
-  (** @param ele element to look for.
-      @return true if tree contains given element.
+  (** 
+    @param ele element to look for.
+    @return true if tree contains given element.
   *)
   let rec contains ele = function
     |Empty -> false
@@ -78,7 +80,6 @@ module Make (Ord: OrderedType) = struct
     @param lst list of elements with types matches tree type.
     @return sorted list.
   *)
-  let sort lst = List.fold_right (fun tree ele -> append tree ele) lst empty 
-                 |> flatten
-  
+  let sort lst = List.fold_right (fun tree ele -> append tree ele) lst empty |> flatten
+
 end
